49d2881c52f68c7b78284b1cc1b7921cee433ecc
Mauriciorpx <mauricio3741@gmail.com>
2024-04-05 17:27:22 -0500

    Archivo nuevo

---
+++ superDigitos.js
@@ -0,0 +1,17 @@
+function sumDigits(n) {
+    // Si n tiene un solo dígito, retornar n
+    if (n < 10) {
+      return n;
+    } else {
+      // Sumar los dígitos de n
+      let suma = 0;
+      suma += n % 10;
+      n = Math.floor(n / 10);
+      // Llamar recursivamente a sumDigits con la suma resultante
+      return suma + sumDigits(n);
+    }
+  }
+
+console.log(sumDigits(12345)); // Resultado: 15
+console.log(sumDigits(987));   // Resultado: 24
+console.log(sumDigits(111));   // Resultado: 3
